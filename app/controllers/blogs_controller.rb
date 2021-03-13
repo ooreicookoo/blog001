42cac521612c973f34dbd0b50692fe945c0106a8
reiko enomoto <ooreicookoo@gmail.com>
2021-03-13 09:42:29 +0900

    strong paramsの設定

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,5 +3,16 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
+  end
+
+  def create
+    Blog.create(blog_params)
+    redirect_to new_blog_path
+  end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
   end
 end
